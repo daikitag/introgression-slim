d60af19ab792d9277e148af4f03022c06e16d144
daikitag <48062118+daikitag@users.noreply.github.com>
2024-04-24 11:25:15 +0100

    Mutation

--- workflow/scripts/plot_block.py
+++ workflow/scripts/plot_block.py
@@ -1,13 +1,9 @@
 import matplotlib.pyplot as plt
-import numpy as np
 import pandas as pd
 
 snk = snakemake  # noqa: F821
 
 df = pd.read_csv(snk.input[0])
-
-# Convert time into forwards in time
-df["time"] = np.max(df["time"]) - df["time"]
 
 plt.figure(figsize=(8, 6))
 plt.plot(df["time"], df["num_block"], marker=".")
@@ -24,5 +20,11 @@ plt.savefig(snk.output.block_length)
 plt.figure(figsize=(8, 6))
 plt.plot(df["time"], df["total_length"], marker=".")
 plt.xlabel("Time t", fontsize=13)
-plt.ylabel("Average total length of surviving blocks", fontsize=13)
+plt.ylabel("Average total length of surviving blocks per individual", fontsize=13)
 plt.savefig(snk.output.total_length)
+
+plt.figure(figsize=(8, 6))
+plt.plot(df["time"], df["num_node"], marker=".")
+plt.xlabel("Time t", fontsize=13)
+plt.ylabel("Number of chromosomes inheriting the block", fontsize=13)
+plt.savefig(snk.output.num_node)
--- workflow/scripts/process_tree.py
+++ workflow/scripts/process_tree.py
@@ -10,42 +10,64 @@ table = ts.tables
 
 num_ind = int(snk.params.N)
 
-# All mutations are happening at the same time, so we can obtain the node for the 1st
-# mutation.
-# Time is in forward simulation
-mutation_node = ts.mutation(0).node
-max_time = int(ts.node(mutation_node).time)
+if ts.num_mutations > 0:
 
-# It would be better to put `plot_block.py` plot code here instead of storing IBD
-# segments.
-# Store IBD segment length information in csv and write another Python code to plot
+    # All mutations are happening at the same time, so we can obtain the node for the 1st
+    # mutation.
+    # Time is in forward simulation
+    mutation_node = ts.mutation(0).node
+    # We will make sure that the mutation occurs at the root time
+    assert ts.node(mutation_node).time == ts.max_root_time
 
+    # It would be better to put `plot_block.py` plot code here instead of storing IBD
+    # segments.
+    # Store IBD segment length information in csv and write another Python code to plot
 
-total_length_average_array = []
-block_length_average_array = []
-num_block_array = []
+    total_length_average_array = []
+    block_length_average_array = []
+    num_block_array = []
+    num_node_array = []
 
-for i in tqdm(range(int(max_time + 1))):
-    # (ts.nodes_time==i) & (ts.nodes_individual != -1)
-    # It is individual -> Guaranteed that it is the node that carries the mutation
-    ibd_table = table.link_ancestors(
-        samples=np.where(ts.nodes_time == i)[0], ancestors=[mutation_node]
-    )
-    total_length = np.sum(ibd_table.right - ibd_table.left)
-    total_length_average = total_length / num_ind
-    block_length_average = np.mean(ibd_table.right - ibd_table.left)
-    num_block = len(ibd_table)
+    for i in tqdm(range(int(ts.max_root_time))):
+        # (ts.nodes_time==i) & (ts.nodes_individual != -1)
+        # It is individual -> Guaranteed that it is the node that carries the mutation
+        node_array = np.where((ts.nodes_time == i) & (ts.nodes_individual != -1))[0]
 
-    total_length_average_array.append(total_length_average)
-    block_length_average_array.append(block_length_average)
-    num_block_array.append(num_block)
+        if node_array.size > 0:
+            ibd_table = table.link_ancestors(
+                samples=node_array, ancestors=[mutation_node]
+            )
+            total_length = np.sum(ibd_table.right - ibd_table.left)
+            total_length_average = total_length / num_ind
+            block_length_average = np.mean(ibd_table.right - ibd_table.left)
+            num_block = len(ibd_table)
+        else:
+            total_length_average = 0
+            block_length_average = 0
+            num_block = 0
+
+        total_length_average_array.append(total_length_average)
+        block_length_average_array.append(block_length_average)
+        num_block_array.append(num_block)
+        num_node_array.append(len(np.unique(ibd_table.child)))
+
+else:
+    total_length_average_array = np.zeros(int(ts.max_root_time))
+    block_length_average_array = np.zeros(int(ts.max_root_time))
+    num_block_array = np.zeros(int(ts.max_root_time))
+    num_node_array = np.zeros(int(ts.max_root_time))
 
 df = pd.DataFrame(
     {
-        "time": np.arange(max_time + 1),
+        "time": np.arange(int(ts.max_root_time)),
         "total_length": total_length_average_array,
         "block_length": block_length_average_array,
         "num_block": num_block_array,
+        "num_node": num_node_array,
     }
 )
+
+# Convert time into forwards in time, where (time=0) is when the simulation starts
+df["time"] = np.max(df["time"]) - df["time"] + 1
+
 df.to_csv(snk.output[0])
